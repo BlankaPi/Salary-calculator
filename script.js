4402628405763a50038b57bc99064c479b359607
BlankaPi <111786704+BlankaPi@users.noreply.github.com>
2022-12-22 14:06:32 +0100

    refactor using class constructor

--- script.js
+++ script.js
@@ -1,260 +1,240 @@
 const form = document.getElementById("calc-form");
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 });
 
 
-// INPUT buttons
-const salaryInput = document.querySelector("#salary");
-const radioButtonsBruttoNetto = document.querySelectorAll('input[name="brutto-netto"]');
-const radioButtonsContract = document.querySelectorAll('input[name="contract-type"]');
-const submitButton = document.querySelector("#submit");
+class SalaryCalculator {
 
-// OUTPUT table
-const nettoOutput = document.querySelector(".amount-netto");
+  constructor(salaryInput) {
+    this.salaryInput = salaryInput;
+  }
+
+  updateValue(salaryValue) {
+    this.salaryValue = salaryValue.currentTarget.value
+    console.log(this.salaryValue);
+  }
+
+  chooseSalaryType(salary) {
+    this.salary = salary;
+    console.log(salary);
+  }
+
+  chooseContractType(contract) {
+    this.contract = contract;
+    console.log(contract);
+  }
+
+  clear() {
+    bruttoOutput.innerHTML = 0;
+    nettoOutput.innerHTML = 0;
+    retirementOutput.innerHTML = 0;
+    disabilityOutput.innerHTML = 0;
+    diseaseOutput.innerHTML = 0;
+    healthOutput.innerHTML = 0;
+    taxOutput.innerHTML = 0;
+  }
+
+  updateTable() {
+    if (!this.salaryValue) {
+      return;
+    }
+    if (this.salary == "brutto") {
+      bruttoOutput.innerHTML = this.salaryValue;
+      this.secondUpdateTable();
+    } else if (this.salary == "netto") {
+      switch (this.contract) {
+        case "uop":
+          bruttoOutput.innerHTML = (this.salaryValue / 0.7476).toFixed(2);
+          break;
+        case "uz":
+          bruttoOutput.innerHTML = (this.salaryValue / 0.7223).toFixed(2);
+          break;
+        case "uod":
+          bruttoOutput.innerHTML = (this.salaryValue / 0.864).toFixed(2);
+          break;
+        case "btob":
+          bruttoOutput.innerHTML = (this.salaryValue / 0.766).toFixed(2);
+          break;
+      }
+      this.secondUpdateTable();
+    }
+  }
+
+  secondUpdateTable() {
+    const values = {
+      uop: [0.7476, 0.0976, 0.015, 0.0245, 0.0776, 0.0376],
+      uz: [0.7223, 0.0976, 0.015, 0, 0.08, 0.0852],
+      uod: [0.864, 0, 0, 0, 0, 0.136],
+      btob: [0.766, 0, 0, 0, 0.0542, 0.1798]
+    };
+
+    function calculate([a, b, c, d, e, f]) {
+      const v = bruttoOutput.innerHTML;
+      if (v != 0) {
+        nettoOutput.innerHTML = (v * a).toFixed(2);
+        retirementOutput.innerHTML = (v * b).toFixed(2);
+        disabilityOutput.innerHTML = (v * c).toFixed(2);
+        diseaseOutput.innerHTML = (v * d).toFixed(2);
+        healthOutput.innerHTML = (v * e).toFixed(2);
+        taxOutput.innerHTML = (v * f).toFixed(2);
+      }
+    };
+
+    if (this.contract == "uop") {
+      calculate(values.uop);
+    } else if (this.contract == "uz") {
+      calculate(values.uz);
+    } else if (this.contract == "uod") {
+      calculate(values.uod);
+    } else if (this.contract == "btob") {
+      calculate(values.btob);
+    };
+  }
+
+  piechart() {
+
+    switch (this.contract) {
+      case "uop":
+        donutChart(donutChartLabels.uop);
+        break;
+      case "uz":
+        donutChart(donutChartLabels.uz);
+        break;
+      case "uod":
+        donutChart(donutChartLabels.uod);
+        break;
+      case "btob":
+        donutChart(donutChartLabels.btob);
+        break;
+    }
+  }
+}
+
+const salaryInput = document.getElementById("salary");
+
 const bruttoOutput = document.querySelector(".amount-brutto");
+const nettoOutput = document.querySelector(".amount-netto");
+
+const salaryType = document.querySelectorAll('input[name="brutto-netto"]');
+const contractType = document.querySelectorAll('input[name="contract-type"]');
+
 const retirementOutput = document.querySelector(".retirement");
 const disabilityOutput = document.querySelector(".disability");
 const diseaseOutput = document.querySelector(".disease");
 const healthOutput = document.querySelector(".health");
 const taxOutput = document.querySelector(".tax");
 
-// OUTPUT donut
-const chart = document.querySelector("#piechart");
-const resultSection = document.querySelector(".result-calculator")
-
-submitButton.addEventListener("click", calculate);
+const submitButton = document.getElementById("submit");
+const resetButton = document.getElementById("reset");
+const resultSection = document.querySelector(".result-calculator");
 
 
-function calculate(){
-  console.log("Calculator working");
-  let s = parseFloat(salaryInput.value);
-  if (isNaN(s)) return;
+const salaryCalculator = new SalaryCalculator(salaryInput, salaryType, contractType);
 
-  for (const button1 of radioButtonsBruttoNetto) {
-    
-    if(button1.checked && button1.value == "brutto"){
-      bruttoOutput.innerHTML = s.toFixed(2);
-      for(const button2 of radioButtonsContract) {
-        if(button2.checked) {
-          switch(button2.value) {
-            case "uop":
-              nettoOutput.innerHTML = (s * 0.7476).toFixed(2);
-            break;
-            case "uz":
-              nettoOutput.innerHTML = (s * 0.7223).toFixed(2);
-              break;
-           case "uod":
-              nettoOutput.innerHTML = (s * 0.864).toFixed(2);
-              break;
-            case "btob":
-              nettoOutput.innerHTML = (s * 0.766).toFixed(2);
-              break;
-          }
-        }
-      }
-    };
+salaryInput.addEventListener("input", e => {
+  salaryCalculator.updateValue(e);
+});
 
-    if(button1.checked && button1.value == "netto"){
-      nettoOutput.innerHTML = s.toFixed(2);
-      for(const button2 of radioButtonsContract) {
-        if(button2.checked) {
-          switch(button2.value) {
-            case "uop":
-              bruttoOutput.innerHTML = (s / 0.7476).toFixed(2);
-            break;
-            case "uz":
-              bruttoOutput.innerHTML = (s / 0.7223).toFixed(2);
-              break;
-           case "uod":
-              bruttoOutput.innerHTML = (s / 0.864).toFixed(2);
-              break;
-            case "btob":
-              bruttoOutput.innerHTML = (s / 0.766).toFixed(2);
-              break;
-          }
-        }
-      }
-    };
+salaryType.forEach((radio) => {
+  radio.addEventListener("click", () => {
+    salaryCalculator.chooseSalaryType(radio.value);
+  })
+});
+
+contractType.forEach((radio) => {
+  radio.addEventListener("click", () => {
+    salaryCalculator.chooseContractType(radio.value);
+  })
+});
+
+submitButton.addEventListener("click", () => {
+  salaryCalculator.updateTable();
+  salaryCalculator.piechart();
+  resultSection.classList.remove("unvisible");
+});
+
+resetButton.addEventListener("click", () => {
+  salaryCalculator.clear();
+  if(resultSection.classList.contains("unvisible") == false) {
+    resultSection.classList.add("unvisible");    
+  } else {
+    console.log("hejka");
+    return;
   };
+});
 
-  for (const button2 of radioButtonsContract) {
-    if(button2.checked) {
-      let v = bruttoOutput.innerHTML;
-      switch(button2.value) {
-        case "uop":
-          retirementOutput.innerHTML = (v * 0.0976).toFixed(2);
-          disabilityOutput.innerHTML = (v * 0.015).toFixed(2);
-          diseaseOutput.innerHTML = (v * 0.0245).toFixed(2);
-          healthOutput.innerHTML = (v * 0.0776).toFixed(2);
-          taxOutput.innerHTML = (v * 0.0376).toFixed(2);
-          break;
-        case "uz":
-          retirementOutput.innerHTML = (v * 0.0976).toFixed(2);
-          disabilityOutput.innerHTML = (v * 0.015).toFixed(2);
-          diseaseOutput.innerHTML = (v * 0).toFixed(2);
-          healthOutput.innerHTML = (v * 0.08).toFixed(2);
-          taxOutput.innerHTML = (v * 0.0852).toFixed(2);
-          break;
-        case "uod":
-          retirementOutput.innerHTML = (0).toFixed(2);
-          disabilityOutput.innerHTML = (0).toFixed(2);
-          diseaseOutput.innerHTML = (0).toFixed(2);
-          healthOutput.innerHTML = (0).toFixed(2);
-          taxOutput.innerHTML = (v * 0.136).toFixed(2);
-          break;
-        case "btob":
-          retirementOutput.innerHTML = (0).toFixed(2);
-          disabilityOutput.innerHTML = (0).toFixed(2);
-          diseaseOutput.innerHTML = (0).toFixed(2);
-          healthOutput.innerHTML = (v * 0.0542).toFixed(2);
-          taxOutput.innerHTML = (v * 0.1798).toFixed(2);
-          break;
+
+// DONUT CHART DATA
+
+function donutChart(data) {
+
+  let donut = new Chart(document.getElementById("doughnut-chart"), {
+    type: 'doughnut',
+    data: {
+      labels: data.labels,
+      datasets: data.datasets
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Procentowy udział:"
+      },
+      legend: {
+        display: false
       }
     }
-  }
-}; 
+  });
 
-// DONUT CHART  
+  return donut;
+};
 
-// CONTRACT TYPE = UoP
-let labelsUoP = [
-  "Ubezpieczenie emerytalne", 
-  "Ubezpieczenie rentowe", 
-  "Ubezpieczenie chorobowe", 
-  "Ubezpieczenie zdrowotne", 
-  "Zaliczka na podatek",
-  "Twoja wypłata"
-];
 
-let dataUoP = [
-  9.76, 
-  1.5,
-  2.45,
-  9,
-  3.76,
-  73.53
-];
 
-let colorsUoP = [
-  "#ADD8E6",
-  "#37AF65",
-  "#FFC0CB",
-  "#DAE75E",
-  "#FFA500",
-  "#1B75BC"
-];
-
-// CONTRACT TYPE = Uz
-let labelsUz = [
-  "Ubezpieczenie emerytalne", 
-  "Ubezpieczenie rentowe", 
-  "Ubezpieczenie zdrowotne", 
-  "Zaliczka na podatek",
-  "Twoja wypłata"
-];
-
-let dataUz = [
-  9.76, 
-  1.5,
-  8,
-  8.52,
-  72.23
-];
-
-let colorsUz = [
-  "#ADD8E6",
-  "#37AF65",
-  "#DAE75E",
-  "#FFA500",
-  "#1B75BC"
-];
-
-// CONTRACT TYPE = UoD
-let labelsUoD = [
-  "Zaliczka na podatek",
-  "Twoja wypłata"
-];
-
-let dataUoD = [
-  13.6,
-  86.4
-];
-
-let colorsUoD = [
-  "#FFA500",
-  "#1B75BC"
-];
-
-// CONTRACT TYPE = btob
-let labelsBtoB = [
-  "Ubezpieczenie zdrowotne", 
-  "Zaliczka na podatek",
-  "Twoja wypłata"
-];
-
-let dataBtoB = [
-  5.42,
-  17.98,
-  76.6
-];
-
-let colorsBtoB = [
-  "#DAE75E",
-  "#FFA500",
-  "#1B75BC"
-];
-
-let donut = new Chart(document.getElementById("doughnut-chart"), {
-  type: 'doughnut',
-  data: {
-    labels: undefined,
+let donutChartLabels = {
+  uop: {
+    labels: ["Ubezpieczenie emerytalne", "Ubezpieczenie rentowe", "Ubezpieczenie chorobowe", "Ubezpieczenie zdrowotne", "Zaliczka na podatek", "Twoja wypłata"],
     datasets: [
       {
-        backgroundColor: undefined,
-        data: undefined
+        backgroundColor: ["#ADD8E6", "#37AF65", "#FFC0CB", "#DAE75E", "#FFA500", "#1B75BC"],
+        data: [9.76, 1.5, 2.45, 9, 3.76, 73.53]
       }
     ]
   },
-  options: {
-    title: {
-      display: true,
-      text: "Procentowy udział:"
-    },
-    legend: {
-      display: false
-    }
-  }
-});
 
-
-submitButton.addEventListener("click", () => {
-  resultSection.classList.remove("unvisible");
-   
-    for(const button2 of radioButtonsContract) {
-      if(button2.checked) {
-        switch (button2.value) {
-          case "uop":
-            donut.data.labels = labelsUoP;
-            donut.data.datasets = [{backgroundColor: colorsUoP, data: dataUoP}];
-            donut.update();
-            break;
-          case "uz":
-            donut.data.labels = labelsUz;
-            donut.data.datasets = [{backgroundColor: colorsUz, data: dataUz}];
-            donut.update();
-            break;
-          case "uod":
-            donut.data.labels = labelsUoD;
-            donut.data.datasets = [{backgroundColor: colorsUoD, data: dataUoD}];
-            donut.update();
-            break;
-          case "btob":
-            donut.data.labels = labelsBtoB;
-            donut.data.datasets = [{backgroundColor: colorsBtoB, data: dataBtoB}];
-            donut.update();
-            break;
-        }
+  uz: {
+    labels: ["Ubezpieczenie emerytalne", "Ubezpieczenie rentowe", "Ubezpieczenie zdrowotne", "Zaliczka na podatek", "Twoja wypłata"],
+    datasets: [
+      {
+        backgroundColor: ["#ADD8E6", "#37AF65", "#DAE75E", "#FFA500", "#1B75BC"],
+        data: [9.76, 1.5, 8, 8.52, 72.23]
       }
-    } 
-});
+    ]
+  },
+
+  uod: {
+    labels: ["Zaliczka na podatek", "Twoja wypłata"],
+    datasets: [
+      {
+        backgroundColor: ["#FFA500", "#1B75BC"],
+        data: [13.6, 86.4]
+      }
+    ]
+  },
+
+  btob: {
+    labels: ["Ubezpieczenie zdrowotne", "Zaliczka na podatek", "Twoja wypłata"],
+    datasets: [
+      {
+        backgroundColor: ["#DAE75E", "#FFA500", "#1B75BC"],
+        data: [5.42, 17.98, 76.6]
+      }
+    ]
+  },
+};
+
+
+
+
+
+
